3af0f5304993ebd6275a1c3af77e541b342db482
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-09-14 20:43:53 +0800

    feat: add zod form validation

--- src/pages/Forms/Register.tsx
+++ src/pages/Forms/Register.tsx
@@ -1,6 +1,8 @@
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Select,
   SelectContent,
@@ -8,95 +10,59 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
 import { Loader2 } from "lucide-react";
-import ErrorCard from "@/components/ErrorCard";
-import api from "../../api";
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+
+import { registerFormSchema, type RegisterInputData } from "@/types/input";
+import api from "@/api";
+
 const Register = () => {
-  const [first_name, setFirstname] = useState<string>("");
-  const [last_name, setLastname] = useState<string>("");
-  const [role, setRole] = useState<string>("")
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [password2, setConfirmPassword] = useState<string>("");
   const [loading, setIsloading] = useState<boolean>(false);
-  const [usernameError, setUsernameError] = useState<string>("");
-  const [passwordError, setPasswordError] = useState<string>("");
+  const [role, setRole] = useState<string>("")
+  const [emailError, setEmailError] = useState<string>("")
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const handleSubmit = async (event: React.FormEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    setIsloading(true);
-    setUsernameError("");
-    setPasswordError("");
-    console.log(first_name, last_name, role, email, password);
+  const { register, formState: {errors}, handleSubmit, setValue} = useForm<RegisterInputData>({
+    resolver: zodResolver(registerFormSchema)
+  });
 
-    api
-      .post("/api/user/register/", {
-        first_name,
-        last_name,
-        role,
-        email,
-        password,
-        password2,
-      })
-      .then((response) => {
-        setIsloading(true);
-        console.log(response);
-        navigate("/login");
-      })
-      .catch((error) => {
-        console.log(error);
-        if (error) {
-          console.error(error);
+  const onSubmit = async (data: RegisterInputData) => {
+    setIsloading(true)
+    setEmailError("")
 
-          setUsernameError(error.response.data.detail);
-          setPasswordError(error.response.data.password);
-        }
-      })
-      .finally(() => {
-        setIsloading(false);
-      });
-  };
-
-  const handleFirstnameChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setFirstname(event.target.value);
-  };
-
-  const handleLastnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLastname(event.target.value);
-  };
-
-  const handleRoleChange = (value: string) => {
-    setRole(value)
-    console.log(`event: ${value }`)
+    api.post('api/user/register/', {
+      first_name: data.first_name, 
+      last_name: data.last_name,
+      role: data.role,
+      email: data.email,
+      password: data.password,
+      password2: data.password2
+    }).then((response) => {
+      console.log(response)
+      navigate('/login')
+    }).catch((error) => {
+      console.log(error)
+      setEmailError(error?.response?.data?.email || "An error occured")
+    }).finally(() => {
+      setIsloading(false)
+    })
   }
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const handleConfirmPasswordChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setConfirmPassword(event.target.value);
-  };
+  const handleRoleChange = (roleValue: string) => {
+    setRole(roleValue);
+    setValue('role',roleValue)
+  }
+  
 
   return (
     <>
       <div className=" h-screen flex justify-center items-center ">
-        <div className=" p-7 rounded border-2 border-slate w-1/4 ">
-          {usernameError && <ErrorCard description={usernameError} />}
+        <form className=" p-7 rounded border-2 border-slate w-1/4 " onSubmit={handleSubmit(onSubmit)}>
+          {errors.email && (<p className="text-red-500 ">{errors.email.message}</p>)}
+          {emailError && (<p className="text-red-500 ">{emailError}</p>)}
 
           <p className="font-extrabold text-xl">Account</p>
           <p className="mt-2 mb-5 text-gray-600">Create an account</p>
@@ -106,9 +72,10 @@ const Register = () => {
           <Input
             id="first-name"
             className="mb-4"
-            onChange={handleFirstnameChange}
-            value={first_name}
+            {...register("first_name")}
           />
+          {errors.first_name && (<p className="text-red-500 ">{errors.first_name.message}</p>)}
+
 
           <Label htmlFor="last-name" className="my-2">
             Last Name
@@ -116,25 +83,26 @@ const Register = () => {
           <Input
             id="last-name"
             className="mb-4"
-            onChange={handleLastnameChange}
-            value={last_name}
-            placeholder=""
+            {...register("last_name")}
           />
+          {errors.last_name && (<p className="text-red-500 ">{errors.last_name.message}</p>)}
+
 
           <Label htmlFor="email" className="my-2">
             Role
           </Label>
-          <Select onValueChange={handleRoleChange} >
+          <Select onValueChange={handleRoleChange}>
             <SelectTrigger className="w-full mb-4">
               <SelectValue placeholder="Select Role" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="Supply Officer">Supply Officer</SelectItem>
-              <SelectItem value="Budget Officer">Budget Officer</SelectItem>
-              <SelectItem value="BAC Officer">BAC Officer</SelectItem>
-              <SelectItem value="Admin">Admin</SelectItem>
+              <SelectItem value="Supply Officer" {...register("role")}>Supply Officer</SelectItem>
+              <SelectItem value="Budget Officer" {...register("role")}>Budget Officer</SelectItem>
+              <SelectItem value="BAC Officer" {...register("role")}>BAC Officer</SelectItem>
+              <SelectItem value="Admin" {...register("role")}>Admin</SelectItem>
             </SelectContent>
           </Select>
+          {errors.role && (<p className="text-red-500 ">{errors.role.message}</p>)}
 
           <Label htmlFor="email" className="my-2">
             Email
@@ -142,10 +110,10 @@ const Register = () => {
           <Input
             id="email"
             className="mb-4"
-            onChange={handleEmailChange}
-            value={email}
+            {...register("email")}
             placeholder="example@gmail.com"
           />
+          {errors.email && (<p className="text-red-500 ">{errors.email.message}</p>)}
 
           <Label htmlFor="password" className="">
             Password
@@ -154,34 +122,32 @@ const Register = () => {
             type="password"
             id="password"
             className="mb-4"
-            onChange={handlePasswordChange}
-            value={password}
+            {...register("password")}
           ></Input>
+          {errors.password && (<p className="text-red-500 ">{errors.password.message}</p>)}
 
           <Label htmlFor="password" className="">
             Confirm Password
           </Label>
           <Input
             type="password"
-            id="password"
-            className=""
-            onChange={handleConfirmPasswordChange}
-            value={password2}
+            id="confirm-password"
+            className=""  
+            {...register("password2")}
           ></Input>
-          <p className="text-red-500 text-sm">
-            {passwordError && passwordError}
-          </p>
+          {errors.password2 && (<p className="text-red-500 ">{errors.password2.message}</p>)}
 
-          <Button className="w-full mt-4" onClick={handleSubmit}>
+
+          <Button type="submit" className="w-full mt-4" >
             {loading ? <Loader2 className="animate-spin" /> : "Register"}
           </Button>
           <p>
             Already have an Account?{" "}
             <span className="text-sky-500">
-              <Link to="/login">Login</Link>
+              <Link to="/">Login</Link>
             </span>
           </p>
-        </div>
+        </form>
       </div>
     </>
   );
