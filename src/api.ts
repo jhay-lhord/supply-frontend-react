7a02dfa63adc6626f3e35e3ebcb2c2af32de2216
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-09-17 14:07:25 +0800

    style: remove unnecesarry comment

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants"; // Assume you store the refresh token as well
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants"; 
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_RENDER_API_URL,
