1ace39db3aee8790b1a4aaf27820a0751946deb7
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-09-25 20:39:25 +0800

    feat: add purchase request input form

--- src/pages/Dashboard/SupplyDashboard/components/PurchaseRequestDataTable.tsx
+++ src/pages/Dashboard/SupplyDashboard/components/PurchaseRequestDataTable.tsx
@@ -5,26 +5,28 @@ import { getPurchaseRequest } from "@/services/purchaseRequestServices";
 import { purchaseRequestType } from "@/types/response/puchase-request";
 import { useState, useEffect } from "react";
 import { PlusIcon } from "@radix-ui/react-icons";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import PurchaseRequestForm from "./PurchaseRequestForm";
 
 export default function PurchaseRequestDataTable() {
   const [purchaseRequest, setPurchaseRequest] = useState<purchaseRequestType[]>(
     []
   );
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [lastPrNo, setLastPrNo] = useState<string | null>(null)
+
+  const handleOpenDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+
 
   useEffect(() => {
     const fetchPurchaseRequest = async () => {
       const response = await getPurchaseRequest();
+
+      const lastPrNo = Array.isArray(response.data) && response.data[response.data.length - 1]
+      setLastPrNo(lastPrNo.pr_no)
+
 
       if (response.status === "success") {
         const responseInArray = Array.isArray(response.data)
@@ -36,117 +38,15 @@ export default function PurchaseRequestDataTable() {
     fetchPurchaseRequest();
   }, []);
 
+  console.log(lastPrNo)
+
   return (
     <>
-      <div className="hidden w-full flex-1 flex-col space-y-8 p-8 md:flex">
-        <div className="flex items-center justify-between space-y-2">
-          <div className="flex justify-between w-full">
-            <h2 className="text-2xl font-bold tracking-tight">
-              Purchase Request
-            </h2>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button className="px-6 bg-orange-200 hover:bg-orange-300 text-black">
-                  <PlusIcon className="mr-2"></PlusIcon>Add
-                </Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px] ">
-                <DialogHeader>
-                  <DialogTitle>Add Purchase Request</DialogTitle>
-                  <DialogDescription>
-                    Make changes to your profile here. Click save when you're
-                    done.
-                  </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                      Name
-                    </Label>
-                    <Input
-                      id="name"
-                      value="Pedro Duarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                      Username
-                    </Label>
-                    <Input
-                      id="username"
-                      value="@peduarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                      Name
-                    </Label>
-                    <Input
-                      id="name"
-                      value="Pedro Duarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                      Username
-                    </Label>
-                    <Input
-                      id="username"
-                      value="@peduarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                      Name
-                    </Label>
-                    <Input
-                      id="name"
-                      value="Pedro Duarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                      Username
-                    </Label>
-                    <Input
-                      id="username"
-                      value="@peduarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                      Name
-                    </Label>
-                    <Input
-                      id="name"
-                      value="Pedro Duarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                      Username
-                    </Label>
-                    <Input
-                      id="username"
-                      value="@peduarte"
-                      className="col-span-3"
-                    />
-                  </div>
-                </div>
-                <DialogFooter>
-                  <Button type="submit">Save changes</Button>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          </div>
-        </div>
+      <div className="hidden w-full flex-col space-y-8 p-8 md:flex">
+        <Button className="px-6 bg-orange-200 w-40 flex-1 hover:bg-orange-300 hover:scale-110 ease-in-out transition duration-300 hover:ease-in text-black" onClick={handleOpenDialog}>
+          <PlusIcon className="mr-2"></PlusIcon>Add
+        </Button>
+        <PurchaseRequestForm isDialogOpen={isDialogOpen} setIsDialogOpen={setIsDialogOpen} lastPrNo={lastPrNo}/>
         <DataTable data={purchaseRequest} columns={columns} />
       </div>
     </>
