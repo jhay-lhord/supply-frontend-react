2feb21c9c8bed3f1bd484a3cd5d98ed9ea76f779
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-09-26 19:11:00 +0800

    feat: modify Purchase Order boilerplate

--- src/pages/Dashboard/SupplyDashboard/PurchaseOrder.tsx
+++ src/pages/Dashboard/SupplyDashboard/PurchaseOrder.tsx
@@ -1,65 +1,14 @@
-import { TooltipProvider } from "@radix-ui/react-tooltip";
-import {
-  LayoutDashboard,
-  FileText,
-  ShoppingCart,
-  FileSpreadsheet,
-  FileSymlink,
-} from "lucide-react";
 import DashboardLayout from "@/pages/Dashboard/shared/Layouts/DashboardLayout";
-import { SideNav } from "@/pages/Dashboard/shared/components/SideNav";
+import SupplySidebar from "./components/SupplySidebar";
 
-const PurchaseRequest: React.FC = () => {
+
+const PurchaseOrder: React.FC = () => {
   return (
     <DashboardLayout>
-      <TooltipProvider>
-        <aside className="hidden md:flex flex-col w-auto h-screen p-2 border-r border-gray-200 sticky top-0 pt-16">
-          <SideNav
-            isCollapsed={false}
-            links={[
-              {
-                title: "Dashboard",
-                label: "",
-                link_to: "/dashboard",
-                icon: LayoutDashboard,
-                variant: "default",
-              },
-              {
-                title: "Purchase Request",
-                label: "",
-                link_to: "/purchase-request",
-                icon: FileText,
-                variant: "default",
-              },
-              {
-                title: "Purchase Order",
-                label: "",
-                link_to: "/purchase-order",
-                icon: ShoppingCart,
-                variant: "default",
-              },
-              {
-                title: "Reports",
-                label: "",
-                link_to: "/reports",
-                icon: FileSpreadsheet,
-                variant: "default",
-              },
-              {
-                title: "Transaction",
-                label: "",
-                link_to: "/transaction",
-                icon: FileSymlink,
-                variant: "default",
-              },
-            ]}
-          />
-        </aside>
-      </TooltipProvider>
-      {/* Main Content */}
-      <h1 className="pt-20 p-4  text-3xl font-bold tracking-tight">Purchase Request</h1>
+      <SupplySidebar />
+      <h1 className="pt-16 text-3xl">Purchase Order</h1>
     </DashboardLayout>
   );
 };
 
-export default PurchaseRequest;
+export default PurchaseOrder;
