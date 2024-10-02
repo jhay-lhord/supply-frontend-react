61edd4f6afe1f57c69f3738b1fa61577401e347d
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-02 18:19:30 +0800

    feat: add delete purchase request service

--- src/services/purchaseRequestServices.ts
+++ src/services/purchaseRequestServices.ts
@@ -1,33 +1,50 @@
-import api from "@/api"
-import { ApiResponse } from "@/types/response/api-response"
-import { purchaseRequestType } from "@/types/response/puchase-request"
-import { handleError, handleSucess } from "@/utils/apiHelper"
+import api from "@/api";
+import { ApiResponse } from "@/types/response/api-response";
+import { purchaseRequestType } from "@/types/response/puchase-request";
+import { handleError, handleSucess } from "@/utils/apiHelper";
 
-export const getPurchaseRequest = async (): Promise<ApiResponse<purchaseRequestType[]>> => {
+export const getPurchaseRequest = async (): Promise<
+  ApiResponse<purchaseRequestType[]>
+> => {
   try {
-    const response = await api.get<purchaseRequestType[]>('/api/purchase-request/');
+    const response = await api.get<purchaseRequestType[]>(
+      "/api/purchase-request/"
+    );
     return handleSucess(response);
   } catch (error) {
-    return handleError(error)
+    return handleError(error);
   }
-}
+};
 
-export const getPurchaseRequestItem = async (): Promise<ApiResponse<purchaseRequestType[]>> => {
+export const getPurchaseRequestItem = async (): Promise<
+  ApiResponse<purchaseRequestType[]>
+> => {
   try {
-    const response = await api.get<purchaseRequestType[]>('/api/purchase-request-item/');
+    const response = await api.get<purchaseRequestType[]>(
+      "/api/purchase-request-item/"
+    );
     return handleSucess(response);
   } catch (error) {
-    return handleError(error)
+    return handleError(error);
   }
-}
+};
 
 export const getPurchaseRequestCount = async (): Promise<number> => {
-  const purchasePurchase = await getPurchaseRequest()
+  const purchasePurchase = await getPurchaseRequest();
 
-  if(purchasePurchase.status === "success"){
-    console.log(purchasePurchase)
+  if (purchasePurchase.status === "success") {
+    console.log(purchasePurchase);
     return purchasePurchase.data?.length || 0;
-
   }
-  return 0
-}
+  return 0;
+};
+
+export const deletePurchaseRequest = async (pr_no: string): Promise<T> => {
+  try {
+    const response = await api.delete(`/api/purchase-request/${pr_no}`);
+    console.log(response);
+    return handleSucess(response);
+  } catch (error) {
+    return handleError(error);
+  }
+};
