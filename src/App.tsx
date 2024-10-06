7a01cec642963fd48f3ee20aa57750ce4075e3e4
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-06 18:55:10 +0800

    feat: add React Query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import Login from "@/pages/Forms/Login";
 import Register from "@/pages/Forms/Register";
 import ProtectedRoutes from "./components/Auth/ProtectedRoute";
@@ -22,8 +21,6 @@ import Budget from "./pages/Dashboard/BudgetDashboard/Budget";
 import BudgetReports from "./pages/Dashboard/BudgetDashboard/BudgetReports";
 import BudgetTransaction from "./pages/Dashboard/BudgetDashboard/BudgetTransaction";
 
-const queryClient = new QueryClient();
-
 const Logout = () => {
   localStorage.clear();
   return <Navigate to="/login" />;
@@ -37,7 +34,6 @@ const RegisterAndLogout = () => {
 const App = () => {
   return (
     <>
-    <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
           <Route
@@ -52,7 +48,7 @@ const App = () => {
           <Route path="/logout" element={<Logout />} />
           <Route path="/register" element={<RegisterAndLogout />} />
           <Route path="*" element={<NotFound />} />
-          
+
           {/* Pages in Supply Dashboard*/}
           <Route path="/purchase-request" element={<PurchaseRequest />} />
           <Route path="/purchase-order" element={<PurchaseOrder />} />
@@ -60,18 +56,23 @@ const App = () => {
           <Route path="/inventory" element={<Inventory />} />
 
           {/* Pages in BAC Dashboard */}
-          <Route path="/request-for-quotation" element={<RequestForQuotation/>}/>
-          <Route path="/abstract-of-quotation" element={<AbstractOfQuotation/>}/>
-          <Route path="/bac-reports" element={<BACReports/>}/>
-          <Route path="/bac-transaction" element={<BACTransaction/>} />
+          <Route
+            path="/request-for-quotation"
+            element={<RequestForQuotation />}
+          />
+          <Route
+            path="/abstract-of-quotation"
+            element={<AbstractOfQuotation />}
+          />
+          <Route path="/bac-reports" element={<BACReports />} />
+          <Route path="/bac-transaction" element={<BACTransaction />} />
 
           {/* Pages in Budget Dashboard */}
-          <Route path="/budget" element={<Budget/>} />
-          <Route path="/budget-reports" element={<BudgetReports/>}/>
-          <Route path="/budget-transaction" element={<BudgetTransaction/>}/>
+          <Route path="/budget" element={<Budget />} />
+          <Route path="/budget-reports" element={<BudgetReports />} />
+          <Route path="/budget-transaction" element={<BudgetTransaction />} />
         </Routes>
       </Router>
-      </QueryClientProvider>
     </>
   );
 };
