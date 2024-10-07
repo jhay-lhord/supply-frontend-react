5f67054b3829ae7604f3e29cac64a681253f61ef
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-07 20:10:50 +0800

    chore: replace icon

--- src/pages/Dashboard/SupplyDashboard/Dashboard.tsx
+++ src/pages/Dashboard/SupplyDashboard/Dashboard.tsx
@@ -13,42 +13,36 @@ import DashboardLayout from "@/pages/Dashboard/shared/Layouts/DashboardLayout";
 import { CalendarDateRangePicker } from "../shared/components/CalendarDateRangePicker";
 import { RecentActivity } from "../shared/components/RecentActivity";
 import { DataTable } from "../shared/components/DataTable";
-import { getPurchaseRequestCount, GetPurchaseRequestItem } from "@/services/purchaseRequestServices";
+import {
+  getPurchaseRequestCount,
+  GetPurchaseRequestItem,
+} from "@/services/purchaseRequestServices";
 import SupplySidebar from "./components/SupplySidebar";
 
 const SupplyDashboard: React.FC = () => {
-  const [purchaseRequestCount, setPurchaseRequestCount] = useState<number | null>(null);
-  const [error, setError] = useState<string | null >(null)
+  const [purchaseRequestCount, setPurchaseRequestCount] = useState<
+    number | null
+  >(null);
+  const [error, setError] = useState<string | null>(null);
 
-  const navigate = useNavigate()
-  const fetchPurchaseRequestItem = async () => {
-
-    const items = await GetPurchaseRequestItem()
-    const filteredPr = items.data?.filter(item => item.pr_no.pr_no === pr_no)
-    const displayItemDescription = filteredPr?.map(item => item.item_no.item_description)
-    console.log(displayItemDescription)
-  }
-
-  fetchPurchaseRequestItem()
-
-
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchPurchaseRequestCount = async () => {
       try {
-        const count = await getPurchaseRequestCount()
-        setPurchaseRequestCount(count)
+        const count = await getPurchaseRequestCount();
+        setPurchaseRequestCount(count);
       } catch (err) {
-        setError('something went wrong')
-        console.error(err, error)
+        setError("something went wrong");
+        console.error(err, error);
       }
-    }
+    };
 
-    fetchPurchaseRequestCount()
-  }, [])
+    fetchPurchaseRequestCount();
+  }, []);
   return (
     <DashboardLayout>
-      <SupplySidebar/>
+      <SupplySidebar />
       {/* Main Content */}
       <ScrollArea className="w-full mt-14">
         <main className=" flex-grow">
@@ -65,31 +59,35 @@ const SupplyDashboard: React.FC = () => {
                 </div>
               </div>
               <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-3">
-                <Card className="hover:bg-orange-200 border-2 border-orange-200" onClick={() => {
-                  navigate('/purchase-request')
-                }}>
+                <Card
+                  className="hover:bg-orange-200 border-2 border-orange-200"
+                  onClick={() => {
+                    navigate("/purchase-request");
+                  }}
+                >
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">
                       Active Request
                     </CardTitle>
                     <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
                       fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                      <path
+                        d="M8.69667 0.0403541C8.90859 0.131038 9.03106 0.354857 8.99316 0.582235L8.0902 6.00001H12.5C12.6893 6.00001 12.8625 6.10701 12.9472 6.27641C13.0319 6.4458 13.0136 6.6485 12.8999 6.80001L6.89997 14.8C6.76167 14.9844 6.51521 15.0503 6.30328 14.9597C6.09135 14.869 5.96888 14.6452 6.00678 14.4178L6.90974 9H2.49999C2.31061 9 2.13748 8.893 2.05278 8.72361C1.96809 8.55422 1.98636 8.35151 2.09999 8.2L8.09997 0.200038C8.23828 0.0156255 8.48474 -0.0503301 8.69667 0.0403541ZM3.49999 8.00001H7.49997C7.64695 8.00001 7.78648 8.06467 7.88148 8.17682C7.97648 8.28896 8.01733 8.43723 7.99317 8.5822L7.33027 12.5596L11.5 7.00001H7.49997C7.353 7.00001 7.21347 6.93534 7.11846 6.8232C7.02346 6.71105 6.98261 6.56279 7.00678 6.41781L7.66968 2.44042L3.49999 8.00001Z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                      ></path>
                     </svg>
                   </CardHeader>
                   <CardContent>
-                    <div className="text-2xl font-bold">{purchaseRequestCount}</div>
-                    <p className="text-xs text-muted-foreground">
-                      +20.1% from last month
-                    </p>
+                    <div className="text-2xl font-bold">
+                      {purchaseRequestCount}
+                    </div>
                   </CardContent>
                 </Card>
                 <Card className="hover:bg-orange-200 border-2 border-orange-200">
@@ -98,25 +96,22 @@ const SupplyDashboard: React.FC = () => {
                       Qoutation
                     </CardTitle>
                     <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
                       fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                      <circle cx="9" cy="7" r="4" />
-                      <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                      <path
+                        d="M4.2 1H4.17741H4.1774C3.86936 0.999988 3.60368 0.999978 3.38609 1.02067C3.15576 1.04257 2.92825 1.09113 2.71625 1.22104C2.51442 1.34472 2.34473 1.51442 2.22104 1.71625C2.09113 1.92825 2.04257 2.15576 2.02067 2.38609C1.99998 2.60367 1.99999 2.86935 2 3.17738V3.1774V3.2V11.8V11.8226V11.8226C1.99999 12.1307 1.99998 12.3963 2.02067 12.6139C2.04257 12.8442 2.09113 13.0717 2.22104 13.2837C2.34473 13.4856 2.51442 13.6553 2.71625 13.779C2.92825 13.9089 3.15576 13.9574 3.38609 13.9793C3.60368 14 3.86937 14 4.17741 14H4.2H10.8H10.8226C11.1306 14 11.3963 14 11.6139 13.9793C11.8442 13.9574 12.0717 13.9089 12.2837 13.779C12.4856 13.6553 12.6553 13.4856 12.779 13.2837C12.9089 13.0717 12.9574 12.8442 12.9793 12.6139C13 12.3963 13 12.1306 13 11.8226V11.8V3.2V3.17741C13 2.86936 13 2.60368 12.9793 2.38609C12.9574 2.15576 12.9089 1.92825 12.779 1.71625C12.6553 1.51442 12.4856 1.34472 12.2837 1.22104C12.0717 1.09113 11.8442 1.04257 11.6139 1.02067C11.3963 0.999978 11.1306 0.999988 10.8226 1H10.8H4.2ZM3.23875 2.07368C3.26722 2.05623 3.32362 2.03112 3.48075 2.01618C3.64532 2.00053 3.86298 2 4.2 2H10.8C11.137 2 11.3547 2.00053 11.5193 2.01618C11.6764 2.03112 11.7328 2.05623 11.7613 2.07368C11.8285 2.11491 11.8851 2.17147 11.9263 2.23875C11.9438 2.26722 11.9689 2.32362 11.9838 2.48075C11.9995 2.64532 12 2.86298 12 3.2V11.8C12 12.137 11.9995 12.3547 11.9838 12.5193C11.9689 12.6764 11.9438 12.7328 11.9263 12.7613C11.8851 12.8285 11.8285 12.8851 11.7613 12.9263C11.7328 12.9438 11.6764 12.9689 11.5193 12.9838C11.3547 12.9995 11.137 13 10.8 13H4.2C3.86298 13 3.64532 12.9995 3.48075 12.9838C3.32362 12.9689 3.26722 12.9438 3.23875 12.9263C3.17147 12.8851 3.11491 12.8285 3.07368 12.7613C3.05624 12.7328 3.03112 12.6764 3.01618 12.5193C3.00053 12.3547 3 12.137 3 11.8V3.2C3 2.86298 3.00053 2.64532 3.01618 2.48075C3.03112 2.32362 3.05624 2.26722 3.07368 2.23875C3.11491 2.17147 3.17147 2.11491 3.23875 2.07368ZM5 10C4.72386 10 4.5 10.2239 4.5 10.5C4.5 10.7761 4.72386 11 5 11H8C8.27614 11 8.5 10.7761 8.5 10.5C8.5 10.2239 8.27614 10 8 10H5ZM4.5 7.5C4.5 7.22386 4.72386 7 5 7H10C10.2761 7 10.5 7.22386 10.5 7.5C10.5 7.77614 10.2761 8 10 8H5C4.72386 8 4.5 7.77614 4.5 7.5ZM5 4C4.72386 4 4.5 4.22386 4.5 4.5C4.5 4.77614 4.72386 5 5 5H10C10.2761 5 10.5 4.77614 10.5 4.5C10.5 4.22386 10.2761 4 10 4H5Z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                      ></path>
                     </svg>
                   </CardHeader>
                   <CardContent>
-                    <div className="text-2xl font-bold">50</div>
-                    <p className="text-xs text-muted-foreground">
-                      +180.1% from last month
-                    </p>
+                    <div className="text-2xl font-bold">comming soon</div>
                   </CardContent>
                 </Card>
                 <Card className="hover:bg-orange-200 border-2 border-orange-200">
@@ -125,24 +120,22 @@ const SupplyDashboard: React.FC = () => {
                       Order in Progress
                     </CardTitle>
                     <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
                       fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <rect width="20" height="14" x="2" y="5" rx="2" />
-                      <path d="M2 10h20" />
+                      <path
+                        d="M2 3.5C2 3.22386 2.22386 3 2.5 3H12.5C12.7761 3 13 3.22386 13 3.5V9.5C13 9.77614 12.7761 10 12.5 10H2.5C2.22386 10 2 9.77614 2 9.5V3.5ZM2 10.9146C1.4174 10.7087 1 10.1531 1 9.5V3.5C1 2.67157 1.67157 2 2.5 2H12.5C13.3284 2 14 2.67157 14 3.5V9.5C14 10.1531 13.5826 10.7087 13 10.9146V11.5C13 12.3284 12.3284 13 11.5 13H3.5C2.67157 13 2 12.3284 2 11.5V10.9146ZM12 11V11.5C12 11.7761 11.7761 12 11.5 12H3.5C3.22386 12 3 11.7761 3 11.5V11H12ZM5 6.5C5 6.22386 5.22386 6 5.5 6H7V4.5C7 4.22386 7.22386 4 7.5 4C7.77614 4 8 4.22386 8 4.5V6H9.5C9.77614 6 10 6.22386 10 6.5C10 6.77614 9.77614 7 9.5 7H8V8.5C8 8.77614 7.77614 9 7.5 9C7.22386 9 7 8.77614 7 8.5V7H5.5C5.22386 7 5 6.77614 5 6.5Z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                      ></path>
                     </svg>
                   </CardHeader>
                   <CardContent>
-                    <div className="text-2xl font-bold">4</div>
-                    <p className="text-xs text-muted-foreground">
-                      +19% from last month
-                    </p>
+                    <div className="text-2xl font-bold">comming soon...</div>
                   </CardContent>
                 </Card>
               </div>
@@ -159,9 +152,7 @@ const SupplyDashboard: React.FC = () => {
                 <Card className="col-span-3 border-2 border-orange-200">
                   <CardHeader className="sticky top-0 rounded-md bg-white z-50">
                     <CardTitle>Recent Activity</CardTitle>
-                    <CardDescription>
-                      You made 265 sales this month.
-                    </CardDescription>
+                    <CardDescription>History as of this month</CardDescription>
                   </CardHeader>
                   <ScrollArea className="h-96">
                     <CardContent className="">
