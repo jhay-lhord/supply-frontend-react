d0c00959641b0108debac96524c8d40d62d0cab9
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-07 19:02:32 +0800

    fix: remove item schema

--- src/types/request/purchase-request.ts
+++ src/types/request/purchase-request.ts
@@ -1,17 +1,5 @@
 import { z } from 'zod';
 
-export const itemFormSchema = z.object({
-  item_no: z.string().min(1, 'Required'),
-  item_property: z.string().min(1, 'Required'),
-  unit: z.string().min(1, 'Required'),
-  item_description: z.string().min(1, 'Required'),
-  quantity: z.string().min(1, 'Required'),
-  unit_cost: z.string().min(1, 'Required'),
-  total_cost: z.string().min(1, 'Required'),
-});
-
-export type ItemData = z.infer<typeof itemFormSchema>;
-
 export const purchaseRequestFormSchema = z.object({
   pr_no: z.string().min(1, 'Required'),
   res_center_code: z.string().min(1, 'Required'),
