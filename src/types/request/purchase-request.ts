cdb44906c912d4258a37cc6fb8c68f84313e69a8
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-08 08:48:46 +0800

    feat: add default value

--- src/types/request/purchase-request.ts
+++ src/types/request/purchase-request.ts
@@ -4,7 +4,7 @@ export const purchaseRequestFormSchema = z.object({
   pr_no: z.string().min(1, 'Required'),
   res_center_code: z.string().min(1, 'Required'),
   purpose: z.string().min(1, 'Required'),
-  status: z.string().min(1, 'Required'),
+  status: z.string().min(1, 'Required').default("pending"),
   requested_by: z.string().min(1, 'Required'),
   approved_by: z.string().min(1, 'Required'),
 });
