159973ac00fb5253d3540a7005f3d48674767412
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-20 15:43:45 +0800

    chore: remove unnecessary logging

--- src/utils/jwtHelper.ts
+++ src/utils/jwtHelper.ts
@@ -2,12 +2,9 @@ import { jwtDecode } from "jwt-decode";
 
 export const getRoleFromToken = (): string => {
   const token = localStorage.getItem("access");
-  console.log(`Tokrn ni siya: ${token}`)
 
   if (token) {
     const decodedToken = jwtDecode<any>(token);
-    console.log(decodedToken)
-    console.log(decodedToken.role);
     return decodedToken.role ;
   }
   return '';
@@ -15,12 +12,9 @@ export const getRoleFromToken = (): string => {
 
 export const getEmailFromToken = (): string => {
   const token = localStorage.getItem("access");
-  console.log(`Tokrn ni siya: ${token}`)
 
   if (token) {
     const decodedToken = jwtDecode<any>(token);
-    console.log(decodedToken)
-    console.log(decodedToken.email);
     return decodedToken.email ;
   }
   return '';
