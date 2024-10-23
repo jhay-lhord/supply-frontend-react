4be169a2e46aef4442807b68444257ffa9e8d96c
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-23 20:44:51 +0800

    fix: remove sheet shadcn components

--- src/pages/Dashboard/SupplyDashboard/components/data-table-row-actions.tsx
+++ src/pages/Dashboard/SupplyDashboard/components/data-table-row-actions.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {TrashIcon, PlusIcon, ArrowTopRightIcon } from "@radix-ui/react-icons";
+import {TrashIcon, ArrowTopRightIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 import {
   AlertDialog,
@@ -17,12 +17,8 @@ import {
   TooltipTrigger,
   TooltipContent,
 } from "@/components/ui/tooltip";
-import { Sheet, SheetContent } from "@/components/ui/sheet";
 import { deletePurchaseRequest } from "@/services/purchaseRequestServices";
 import { purchaseRequestType } from "@/types/response/puchase-request";
-import ItemForm from "./ItemForm";
-import { useItem } from "@/services/itemServices";
-import { itemType } from "@/types/response/item";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { Separator } from "@/components/ui/separator";
@@ -37,10 +33,7 @@ export const DataTableRowActions = ({
   _data,
   pr_no,
 }: DataTableRowActionsProps) => {
-  const [isSheetOpen, setIsSheetOpen] = useState<boolean>(false);
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
-  const [isItemDialogOpen, setIsItemDialogOpen] = useState<boolean>(false);
-  const [prNo, setPrNo] = useState<string | undefined>("");
 
   const queryClient = useQueryClient();
   const navigate = useNavigate();
@@ -55,23 +48,9 @@ export const DataTableRowActions = ({
     },
   });
 
-  const { isLoading, error, data } = useItem();
-
-  if (isLoading) return <div>Loading ...</div>;
-
-  if (error) return <div>Error Occured: {error.message}</div>;
-
   const handleViewClick = (event: React.MouseEvent) => {
     event.stopPropagation();
     navigate(`/purchase-request/${pr_no}`);
-  };
-
-  const handleAddItemsClick = (event: React.MouseEvent) => {
-    event.stopPropagation();
-    console.log(`The Pr No: ${pr_no}`);
-    setIsItemDialogOpen(true);
-    setPrNo(pr_no);
-    console.log(`Final Pr Number: ${prNo}`);
   };
 
   const handleOpenDialog = (event: React.MouseEvent) => {
@@ -112,22 +91,6 @@ export const DataTableRowActions = ({
           <Tooltip>
             <TooltipTrigger asChild>
               <Button
-                onClick={handleAddItemsClick}
-                variant="ghost"
-                className="flex h-8 w-8 p-0 data-[state=open]:bg-muted hover:rounded-full"
-              >
-                <PlusIcon className="h-4 w-4 text-orange-400" />
-                <span className="sr-only">Add Item</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="top">Add Item</TooltipContent>
-          </Tooltip>
-
-          <Separator className="h-8" orientation="vertical"/>
-
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
                 onClick={handleOpenDialog}
                 variant="ghost"
                 className="flex h-8 w-8 p-0 data-[state=open]:bg-muted hover:rounded-full text-orange-400 hover:bg-red-400 hover:text-gray-100"
@@ -140,21 +103,6 @@ export const DataTableRowActions = ({
           </Tooltip>
         </div>
       </TooltipProvider>
-
-      <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
-        <SheetContent className="w-[60rem] ">
-          <h1 className="text-2xl my-5">Items</h1>
-          <p>
-            <span className="font-bold">PR number:</span> {_data?.pr_no}
-          </p>
-          {data &&
-            data.data
-              ?.filter(
-                (item: itemType) => item.purchase_request === _data.pr_no
-              )
-              .map((item) => <p key={item.item_no}>{item.item_description}</p>)}
-        </SheetContent>
-      </Sheet>
 
       <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
         <AlertDialogContent>
@@ -179,11 +127,6 @@ export const DataTableRowActions = ({
         </AlertDialogContent>
       </AlertDialog>
 
-      <ItemForm
-        isItemDialogOpen={isItemDialogOpen}
-        setIsItemDialogOpen={setIsItemDialogOpen}
-        pr_no={prNo!}
-      />
     </>
   );
 };
