46ab4db6212b8286e74747abb520833b49554381
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-25 16:41:44 +0800

    style: apply code formatting

--- src/pages/Dashboard/shared/components/UserNav.tsx
+++ src/pages/Dashboard/shared/components/UserNav.tsx
@@ -1,46 +1,43 @@
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { useNavigate } from "react-router-dom"
-import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
-import { getRoleFromToken } from "@/utils/jwtHelper"
-import { getEmailFromToken } from "@/utils/jwtHelper"
+import {
+  getRoleFromToken,
+  getEmailFromToken,
+  getFullnameFromToken,
+} from "@/utils/jwtHelper";
 
 export const UserNav = () => {
   const userRole = getRoleFromToken();
   const userEmail = getEmailFromToken();
+  const userFullname = getFullnameFromToken();
   const trimmedUserRole = userRole
-  .split(' ')                  
-  .map(word => word[0])       
-  .join(''); 
+    .split(" ")
+    .map((word) => word[0])
+    .join("");
 
-  console.log(trimmedUserRole)
   const navigate = useNavigate();
   const handleClick = () => {
-    localStorage.clear()
-    navigate('/')
-  }
-
-  console.log(userRole)
-
-
+    localStorage.clear();
+    navigate("/");
+  };
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-10 w-10  rounded-full p-2 ">
+        <Button
+          variant="ghost"
+          className="relative h-10 w-10  rounded-full p-2 "
+        >
           <Avatar className="h-10 w-10">
             <AvatarImage src="/avatars/01.png" alt="@shadcn" />
             <AvatarFallback>{trimmedUserRole}</AvatarFallback>
@@ -50,18 +47,18 @@ export const UserNav = () => {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{userRole}</p>
+            <p className="text-base font-medium leading-none">{userRole}</p>
+            <p className="text-sm">{userFullname}</p>
             <p className="text-xs leading-none text-muted-foreground">
               {userEmail}
             </p>
           </div>
         </DropdownMenuLabel>
-        <DropdownMenuSeparator/>
+        <DropdownMenuSeparator />
         <DropdownMenuItem onClick={handleClick}>
           Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
