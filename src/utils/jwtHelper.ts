e30f78f3d3fdc2f6eeb9d04721f9aabba55dfe3c
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-10-25 18:07:10 +0800

    style: apply code formatting

--- src/utils/jwtHelper.ts
+++ src/utils/jwtHelper.ts
@@ -3,29 +3,25 @@ import { jwtDecode } from "jwt-decode";
 const token = localStorage.getItem("access");
 
 export const getRoleFromToken = (): string => {
-
   if (token) {
     const decodedToken = jwtDecode<any>(token);
-    return decodedToken.role ;
+    return decodedToken.role;
   }
-  return '';
+  return "";
 };
 
 export const getEmailFromToken = (): string => {
-
   if (token) {
     const decodedToken = jwtDecode<any>(token);
-    return decodedToken.email ;
+    return decodedToken.email;
   }
-  return '';
+  return "";
 };
 
 export const getFullnameFromToken = (): string => {
-
   if (token) {
     const decodedToken = jwtDecode<any>(token);
-    return decodedToken.fullname ;
+    return decodedToken.fullname;
   }
-  return '';
+  return "";
 };
-
