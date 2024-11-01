fc3c7da1646cfb8476f9ccb6e02264f96e4dabe5
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-11-01 16:16:47 +0800

    fix: add list of chairmans

---
+++ src/pages/Dashboard/SupplyDashboard/data/list-of-chairman.tsx
@@ -0,0 +1,27 @@
+export const chairmans = [
+  {
+    name: "KINTANAR, FITZGERALD C.",
+    department: "College of Education",
+    designation: "Asst. Prof. III/ Dean, COEd/Chair, MAEd Programs"
+  },
+  {
+    name: "VELASCO, LEMUEL M.",
+    department: "College of Education",
+    designation: "Asst. Prof. IV / COEd Sports Coor."
+  }, 
+  {
+    name: "ALBARRACIN, VIRGINIA S.",
+    department: "College of Education",
+    designation: "Asst. Prof. III/ Dean, COEd/Chair, MAEd Programs"
+  },
+  {
+    name: "ALEGUEN, JUDITH M.",
+    department: "College of Education",
+    designation: "Instructor I/ ST Coor & Intl Coor., BSEd Math"
+  },
+  {
+    name: "BEJAGAN, JIMA S.",
+    department: "College of Education",
+    designation: "Asst. Prof. III/ Chair, BTLED"
+  },
+]
