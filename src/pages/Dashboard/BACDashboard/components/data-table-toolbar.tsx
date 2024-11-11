431c22926780f2843ad89513f007b43bb0189ed8
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-11-11 20:47:31 +0800

    feat: add global filtering

--- src/pages/Dashboard/BACDashboard/components/data-table-toolbar.tsx
+++ src/pages/Dashboard/BACDashboard/components/data-table-toolbar.tsx
@@ -23,11 +23,9 @@ export function DataTableToolbar<TData>({
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          placeholder="Filter tasks..."
-          value={(table.getColumn("pr_no")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("pr_no")?.setFilterValue(event.target.value)
-          }
+          placeholder="Filter PR No, Status and more..."
+          value={(table.getState().globalFilter as string) ?? ""}
+          onChange={(event) => table.setGlobalFilter(event.target.value)}
           className="h-8 w-[150px] lg:w-[250px]"
         />
         {table.getColumn("status") && (
