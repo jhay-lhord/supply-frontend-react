80f2723ad193c1964b234cf6b3d56d220f63d58e
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-11-18 19:48:33 +0800

    feat: filter only the purchase request that has request of quotation

--- src/pages/Dashboard/BACDashboard/components/PurchaseRequestInProgressDataTable.tsx
+++ src/pages/Dashboard/BACDashboard/components/PurchaseRequestInProgressDataTable.tsx
@@ -1,21 +1,28 @@
+import { useRequestForQoutation } from "@/services/requestForQoutationServices";
 import Loading from "../../shared/components/Loading";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import { usePurchaseRequestInProgress } from "@/services/purchaseRequestServices";
-import { purchaseRequestType } from "@/types/response/puchase-request";
-
+import { useMemo } from "react";
 
 export default function PurchaseRequestInProgressDataTable() {
-  const {purchaseRequestInProgress, isLoading} = usePurchaseRequestInProgress()
+  const { purchaseRequestInProgress, isLoading } =
+    usePurchaseRequestInProgress();
+  const { data } = useRequestForQoutation();
 
+  const dataHasRFQ = useMemo(() => {
+    const data_ = Array.isArray(data?.data) ? data?.data : []
+    const prNos = data_.map(data => data.purchase_request)
 
-  if (isLoading) return <Loading/>
+    return purchaseRequestInProgress.filter(data => prNos.includes(data.pr_no))
+  }, [data?.data, purchaseRequestInProgress])
 
-  const purchaseRequestInProgressData: purchaseRequestType[] = Array.isArray(purchaseRequestInProgress) ? purchaseRequestInProgress : []
+  if (isLoading) return <Loading />;
+
   return (
     <>
       <div className="hidden w-full flex-col space-y-8 p-4 md:flex">
-        <DataTable data={purchaseRequestInProgressData} columns={columns} />
+        <DataTable data={dataHasRFQ} columns={columns} />
       </div>
     </>
   );
