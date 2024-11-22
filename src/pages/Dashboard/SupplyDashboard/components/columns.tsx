f5c5bf339c197171e333f0202d8a9e3b087b5cee
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-11-22 17:16:07 +0800

    style: add font style

--- src/pages/Dashboard/SupplyDashboard/components/columns.tsx
+++ src/pages/Dashboard/SupplyDashboard/components/columns.tsx
@@ -38,7 +38,7 @@ export const columns: ColumnDef<purchaseRequestType>[] = [
       return (
         <div className="flex space-x-2">
           <span className="max-w-[500px] truncate font-medium">
-            {row.getValue("pr_no")}
+            <p className="font-thin">{row.getValue("pr_no")}</p>
           </span>
         </div>
       );
