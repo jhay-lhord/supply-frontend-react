6477750ad629ec476c8011acfbc6119693de92e8
Jaylord Manila <jaylordmanila.tech@gmail.com>
2024-12-18 16:40:04 +0800

    feat: add stocks

--- src/pages/Dashboard/SupplyDashboard/Stocks.tsx
+++ src/pages/Dashboard/SupplyDashboard/Stocks.tsx
@@ -1,11 +1,49 @@
+import {
+  Package,
+  ShoppingCart,
+  DollarSign,
+  Truck,
+  BarChart2,
+} from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Layout from "./components/Layout/SupplyDashboardLayout";
+import { useGetAllPurchaseOrder, useGetItemsDelivered } from "@/services/puchaseOrderServices";
+import { DataTable } from "./components/data-table";
+import { inventoryColumns } from "./components/inventory-column";
 
-const Stocks: React.FC = () => {
+
+export default function Stocks() {
+
+  const { data } = useGetAllPurchaseOrder()
+  const { data:items_delivered} = useGetItemsDelivered()
+
+  console.log(data?.data)
+  const itemDeliveredData = Array.isArray(items_delivered?.data) ? items_delivered.data : []
+
+  const flattenedItemDeliveredData = itemDeliveredData.map(item => ({
+    ...item,
+    po_no: item.inspection_details.po_details.po_no,
+    item_description: item.item_details.item_quotation_details.item_details.item_description,
+    unit: item.item_details.item_quotation_details.item_details.unit,
+    quantity: item.item_details.item_quotation_details.item_details.quantity,
+    unit_cost: item.item_details.item_quotation_details.item_details.unit_cost,
+  }));
+  
+  console.log(itemDeliveredData)
+
+
   return (
     <Layout>
-      <h1>Stocks</h1>
+      <div className="container mx-auto p-6">
+        <h1 className="text-3xl font-bold mb-6 flex items-center">
+          <Package className="mr-2" />
+          Stocks Management
+        </h1>
+
+        <div className="rounded-md border">
+          <DataTable data={flattenedItemDeliveredData} columns={inventoryColumns}/>
+        </div>
+      </div>
     </Layout>
   );
-};
-
-export default Stocks;
+}
